60e8808c39cb860a148b7f7e26b690ec6cb2704f
Kavya Gupta <35874192+kavyagupta1107@users.noreply.github.com>
2019-06-21 01:33:31 +0530

    Add files via upload

--- irispy.py
+++ irispy.py
@@ -19,10 +19,26 @@ dataset=pandas.read_csv(file,names=names)
 #print(dataset.head(30))
 #print(dataset.describe())
 #print(dataset.groupby('class').size())
-dataset.hist()
-plt.show()
-plt.savefig('histogram.png')
+array=dataset.values
+#keeping aise 20% of data for testing(validation dataset) and rest as training set
+X=array[:,0:4]
+Y=array[:,4]
+validation_size=0.20
+seed=7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+scoring='accuracy'
+# just for checking accuracy
+name='LR' #logistic regression
+model=LogisticRegression(solver='liblinear', multi_class='ovr')
+kfold = model_selection.KFold(n_splits=10, random_state=seed)# 10 fold cross validation-----
+cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+print(msg)
 
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-plt.show()
-plt.savefig('box_&_whisker.png')
+#making predictions on validation dataset
+lr=LogisticRegression(solver='liblinear', multi_class='ovr')
+lr.fit(X_train,Y_train)
+pred=lr.predict(X_validation)
+print(accuracy_score(Y_validation, pred))
+print(confusion_matrix(Y_validation, pred))
+print(classification_report(Y_validation, pred))
